ee95e15fe28a807ae45020833d7ab4c6599240cf
Aditya Jha <adi4ant@gmail.com>
2023-05-29 09:48:13 +0530

    Initialized Repo and commited initial changes

---
+++ listcomprehension.py
@@ -0,0 +1,23 @@
+# Purpose: To implement list comprehension in python
+
+# List comprehension is a way to create a list using a single line of code  
+
+# Example 1: Create a list of squares from 1 to 10
+squares = [i**2 for i in range(1,11)] 
+
+# Example 2: Create a list of even numbers from 1 to 10
+even = [i for i in range(1,11) if i % 2 == 0]
+
+# Create a map from list comprehension 
+# Example 3: Create a map of squares from 1 to 10
+squares_map = {i:i**2 for i in range(1,11)}
+
+
+
+def main():
+    # create logic to implement krushkal tree algorithm
+    
+    
+
+if __name__ == "__main__":
+    main()
